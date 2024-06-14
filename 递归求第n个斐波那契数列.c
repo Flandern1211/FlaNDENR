197fe94a941d25509495c58356a8c8e704875286
Flandern1211 <163982805+Flandern1211@users.noreply.github.com>
2024-06-14 21:24:47 +0800

    Add files via upload

---
+++ 递归求第n个斐波那契数列.c
@@ -0,0 +1,39 @@
+//
+//#define _CRT_SECURE_NO_WARNINGS
+//#include<stdio.h>
+//int fei(int x)
+//{
+//	/*(1)µÝ¹é
+//	if (x <= 2)
+//	{
+//		return 1;
+//
+//	}
+//	else
+//	{
+//		return (fei(x - 1) + fei(x - 2));
+//	}*/
+//	
+//	/*(2)Ñ­»·
+//	int a = 1;
+//	int b = 1;
+//	int c = 1;
+//	while(x>2)
+//	{
+//		c = a + b;
+//		a = b;
+//		b = c;
+//		x--;
+//	}
+//	return c;*/
+//}
+
+
+	//int main()
+	//{
+	//	int num = 0;
+	//	scanf("%d", &num);
+	//	int z = fei(num);
+	//	printf("%d", z);
+	//	return 0;
+	//}
